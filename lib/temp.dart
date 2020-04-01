38e5ed48a4f1ef7ab34b957814a51a3416f847e7
Giovanni Crudele <giovannic@onmsys.onmicrosoft.com>
2020-04-01 18:38:03 +0200

    Primo esmpio di CustomScrollview

---
+++ lib/temp.dart
@@ -0,0 +1,18 @@
+/*
+Center(
+child: Column(
+mainAxisAlignment: MainAxisAlignment.center,
+children: <Widget>[
+Text(
+'You have pushed the button this many times:',
+),
+Text(
+'$_counter',
+style: Theme.of(context).textTheme.display1,
+),
+],
+),
+),
+
+*=
+ */
